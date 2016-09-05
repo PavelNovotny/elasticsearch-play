106695c29919afdbebcc27497764439eecb82a40
Pavel Novotny <pavel.novotny.cz@gmail.com>
2016-09-05 15:20:48 +0200

    oprava chyby expandPath pro undefined nebo null

--- test/test.js
+++ test/test.js
@@ -8,5 +8,8 @@ var index = require("../lib/index.js");
 console.log(index.expandPath("$SCRIPT_DIR/data/bulk.txt"));
 console.log(index.expandPath("/rrr/bbb/$SCRIPT_DIR/dathhhhha/bulk.txt"));
 console.log(index.expandPath("/rrr/bbb/dathhhxxxxxhha/bulk.txt"));
+console.log(index.expandPath("/afsdfds_afdfsd"));
+console.log(index.expandPath(undefined));
+console.log(index.expandPath(null));
 console.log(index.expandPath("$SCRIPT_DIR"));
 
