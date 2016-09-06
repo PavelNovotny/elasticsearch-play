4f50abcdbd2b3348070dc5dec46a2cee46a2b5fe
x0503899 <Milos.Frydl@o2.cz>
2016-09-06 13:08:33 +0200

    pridan offset

--- rms/customer_account_index/generateData.sh
+++ rms/customer_account_index/generateData.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 export SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 echo "$SCRIPT_DIR"
-node /home/elastic/node-apps/dummy-json-play/lib/index.js $SCRIPT_DIR/data/bulk_template.json  $SCRIPT_DIR/data/bulk.txt
+
+batch_count=1
+for (( i=1; i <= $batch_count ; i++ ))
+do
+	node /home/elastic/node-apps/dummy-json-play/lib/index.js $SCRIPT_DIR/data/bulk_template.json  $SCRIPT_DIR/data/bulk$i.txt $i
+done
 
--- rms/customer_account_index/loadData.sh
+++ rms/customer_account_index/loadData.sh
@@ -3,5 +3,8 @@
 export SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 echo "$SCRIPT_DIR"
 
-node elastic.js --batch=$SCRIPT_DIR/data/bulk.post
-
+batch_count=1
+for (( i=1; i <= $batch_count ; i++ ))
+do
+	node elastic.js --batch=$SCRIPT_DIR/data/bulk$1.post
+done
